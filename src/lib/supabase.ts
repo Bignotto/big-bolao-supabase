8fce12653c629d63087b75e43f1fb62b8beb8cac
Thiago Bignotto <bignotto@gmail.com>
2022-05-03 18:54:11 -0300

    added and configured babel plugin inline dotenv

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,10 +2,10 @@ import { createClient } from "@supabase/supabase-js";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //FIX: use babel in line dot env
-const supabaseUrl = "";
-const supabaseAnonKey = "";
+const { SUPABASE_URL } = process.env;
+const { SUPABASE_ANON_KEY } = process.env;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL!, SUPABASE_ANON_KEY!, {
   localStorage: AsyncStorage,
   detectSessionInUrl: false,
 });
